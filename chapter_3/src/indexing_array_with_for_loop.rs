7d5432c881a0adb77832bed2ca291f2bf038a0eb
Abdulaziz Kamil <akdatti94@gmail.com>
2023-12-06 00:59:27 +0100

    renamed chapter_3

---
+++ chapter_3/src/indexing_array_with_for_loop.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let x = [11, 12, 13, 14, 15];
+
+    let mut y = 0;
+
+    while y < 5 {
+        println!("The item is {}", x[y]);
+
+        y = y + 1;
+    }
+
+    println!("LISTOFF!!!");
+}
