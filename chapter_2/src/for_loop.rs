4af4fab2c71e1941beed27ddaa27cef52e235576
Abdulaziz Kamil <akdatti94@gmail.com>
2023-12-06 01:01:12 +0100

    mv wahala

--- chapter_2/src/for_loop.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    let x = [11, 12, 13, 14, 15];
-
-    for element in x.iter() {
-        println!("The value is: {}",element);
-    }
-}
--- chapter_2/src/for_loop_with_range.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-
-    //In case you want to print them in ascending order, remove .rev() and the () as well
-    for element in (1..4).rev() {
-        println!("The item is: {}", element);
-    }
-    println!("LISTOFF!!!");
-}
